5474dbddfc43298115b63b063342f39d77030215
Assaf Benarosh <assaf@orca.security>
2024-12-18 09:42:34 +0200

    WL-3202-gcp-list-buckets

--- gcp_org_resource_count.sh
+++ gcp_org_resource_count.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 LOG_FILE='gcp_resource_count.log'
-exec 2> >(while IFS= read -r line; do echo "$(date '+%Y-%m-%d %H:%M:%S') [stderr] $line"; done >> $LOG_FILE)
+exec 2> >(while IFS= read -r line; do echo "$(date '+%Y-%m-%d %H:%M:%S') [stderr] $line"; done >>$LOG_FILE)
+
+MAX_DB_SIZE_GB=1024
 
 WORKLOAD_VM_UNITS=1
 WORKLOAD_FUNCTION_UNITS=50
@@ -9,150 +11,254 @@ WORKLOAD_SERVERLESS_CONTAINER_UNITS=10
 WORKLOAD_VM_IMAGE_UNITS=1
 WORKLOAD_CONTAINER_IMAGE_UNITS=10
 WORKLOAD_CONTAINER_HOST_UNITS=1
-
-ORG_ID=""
-
-while [[ $# -gt 0 ]]
-do
-    key="$1"
-    case $key in
-        -g|--organization_id)
-        ORG_ID="$2"
-        shift # past argument
-        shift # past value
-        ;;
-        *)    # unknown option
-        shift # past argument
-        ;;
-    esac
-done
-
-if [ -z "$ORG_ID" ]; then
-    echo "[+] Getting organizations list"
-    ORGS=$(gcloud organizations list --format="value(name)")
-    ORGS_LEN=$(echo "$ORGS" | wc -l)
-    echo "Found ${ORGS_LEN} organizations"
-    echo "[+] Iterating organizations and counting resources"
-else
-    ORGS="$ORG_ID"
-    echo "[+] counting resources for organizations id ${ORG_ID}"
-fi
-echo
-
-counter=0
-total_vms=0
-total_functions=0
-total_cloud_run=0
-total_container_images=0
-total_vm_images=0
-total_gke_nodes=0
-total_buckets=0
-total_private_buckets=0
-active_user=$(gcloud config get-value account)
+WORKLOAD_MANAGED_DB_UNITS=1
+WORKLOAD_DATAWAREHOUSE_UNITS=4
+WORKLOAD_BUCKET=1
 
 # listed here https://cloud.google.com/asset-inventory/docs/asset-types
-asset_types="compute.googleapis.com/Instance,cloudfunctions.googleapis.com/CloudFunction,run.googleapis.com/Service,containerregistry.googleapis.com/Image,compute.googleapis.com/Image,k8s.io/Node,storage.googleapis.com/Bucket"
+declare -A RESOURCE_TYPES=(
+        ["compute.googleapis.com/Instance"]="total_vms"
+        ["cloudfunctions.googleapis.com/CloudFunction"]="total_functions"
+        ["run.googleapis.com/Service"]="total_cloud_run"
+        ["containerregistry.googleapis.com/Image"]="total_container_images"
+        ["compute.googleapis.com/Image"]="total_vm_images"
+        ["k8s.io/Node"]="total_gke_nodes"
+        ["sqladmin.googleapis.com/Instance"]="total_managed_db_count"
+        ["bigquery.googleapis.com/Dataset"]="total_datawarehouses_datasets"
+        ["storage.googleapis.com/Bucket"]="total_buckets"
+)
 
-for org_id in $ORGS; do
-  echo "Processing organization: $org_id"
+calculate_resource_workloads() {
+    vm_workloads=$(calculate_workloads "$total_vms" "$WORKLOAD_VM_UNITS")
+    function_workloads=$(calculate_workloads "$total_functions" "$WORKLOAD_FUNCTION_UNITS")
+    container_workloads=$(calculate_workloads "$total_cloud_run" "$WORKLOAD_SERVERLESS_CONTAINER_UNITS")
+    container_image_workloads=$(calculate_workloads "$total_container_images" "$WORKLOAD_CONTAINER_IMAGE_UNITS")
+    vm_image_workloads=$(calculate_workloads "$total_vm_images" "$WORKLOAD_VM_IMAGE_UNITS")
+    container_host_workloads=$(calculate_workloads "$total_gke_nodes" "$WORKLOAD_CONTAINER_HOST_UNITS")
+    managed_db_workloads=$(calculate_workloads "$total_managed_db_count" "$WORKLOAD_MANAGED_DB_UNITS")
+    datawarehouse_workloads=$(calculate_workloads "$total_datawarehouses_datasets" "$WORKLOAD_DATAWAREHOUSE_UNITS")
+    total_bucket_workloads=$(calculate_workloads "$total_buckets" "$WORKLOAD_BUCKET")
+    private_bucket_workloads=$(calculate_workloads "$total_private_buckets" "$WORKLOAD_BUCKET")
 
-  all_resources=$(gcloud asset search-all-resources --scope="organizations/$org_id" --format="json(displayName, assetType, project)" --asset-types="$asset_types")
-  curr_vms=$(echo "$all_resources" | jq -r '.[] | select(.assetType == "compute.googleapis.com/Instance") | .displayName' | wc -l)
-  curr_functions=$(echo "$all_resources" | jq -r '.[] | select(.assetType == "cloudfunctions.googleapis.com/CloudFunction") | .displayName' | wc -l)
-  curr_cloud_run=$(echo "$all_resources" | jq -r '.[] | select(.assetType == "run.googleapis.com/Service") | .displayName' | wc -l)
-  curr_container_images=$(echo "$all_resources" | jq -r '.[] | select(.assetType == "containerregistry.googleapis.com/Image") | .displayName' | wc -l)
-  curr_vm_images=$(echo "$all_resources" | jq -r '.[] | select(.assetType == "compute.googleapis.com/Image") | .displayName' | wc -l)
-  curr_gke_nodes=$(echo "$all_resources" | jq -r '.[] | select(.assetType == "k8s.io/Node") | .displayName' | wc -l)
-  curr_buckets=$(echo "$all_resources" | jq -r '.[] | select(.assetType == "storage.googleapis.com/Bucket") | .displayName' | wc -l)
+    total_workloads=$((vm_workloads + function_workloads + container_workloads + \
+        container_image_workloads + vm_image_workloads + container_host_workloads + managed_db_workloads + datawarehouse_workloads + total_bucket_workloads))
+}
 
-  total_vms=$((total_vms + curr_vms))
-  total_functions=$((total_functions + curr_functions))
-  total_cloud_run=$((total_cloud_run + curr_cloud_run))
-  total_container_images=$((total_container_images + curr_container_images))
-  total_vm_images=$((total_vm_images + curr_vm_images))
-  total_gke_nodes=$((total_gke_nodes + curr_gke_nodes))
-  total_buckets=$((total_buckets + curr_buckets))
+display_summary() {
+    echo "Virtual Machines Count: $total_vms (Workload Units: ${vm_workloads})"
+    echo "Serverless Functions Count: $total_functions (Workload Units: ${function_workloads})"
+    echo "Serverless Containers Count: $total_cloud_run (Workload Units: ${container_workloads})"
+    echo "Container Images Count: $total_container_images (Workload Units: ${container_image_workloads})"
+    echo "VM Images Count: $total_vm_images (Workload Units: ${vm_image_workloads})"
+    echo "Container Hosts Count: $total_gke_nodes (Workload Units: ${container_host_workloads})"
+    echo "Managed Databases Count (up to $((MAX_DB_SIZE_GB / 1024)) TB): $total_managed_db_count (Workload Units: ${managed_db_workloads})"
+    echo "DataWarehouses Count: $total_datawarehouses_datasets (Workload Units: $datawarehouse_workloads)"
+    echo "Private Buckets: $total_private_buckets (Workload Units: $private_bucket_workloads)"
+    echo "Public Buckets: $((total_buckets - total_private_buckets)) (Workload Units: $((total_bucket_workloads - private_bucket_workloads)))"
+    echo "Buckets Count: $total_buckets (Workload Units: $total_bucket_workloads)"
+    echo "--------------------------------------"
+    echo "TOTAL Estimated Workload Units: ${total_workloads}"
+}
 
+parse_arguments() {
+    local org_id=""
 
+    # Parse command-line options
+    while [[ $# -gt 0 ]]; do
+          key="$1"
+        case $key in
+        -g | --organization_id)
+                org_id="$2"
+            shift # past argument
+            shift # past value
+            ;;
+        -s | --max-db-size-gb)
+            MAX_DB_SIZE_GB="$2"
+            shift # past argument
+            shift # past value
+            ;;
+        *)    # unknown option
+            shift # past argument
+            ;;
+        esac
+    done
 
-  is_public_access_prevention_enabled_from_org_level=$(gcloud resource-manager org-policies describe storage.publicAccessPrevention --organization="$org_id" --effective --format json |  jq '.booleanPolicy.enforced == true')
+    echo "$org_id"
+}
 
-  if [[ $is_public_access_prevention_enabled_from_org_level == "true" ]]; then
-      total_private_buckets=$((total_private_buckets + curr_buckets))
-  else
-      current_all_buckets_json=$(gcloud projects list --filter="parent.id=$org_id" --format="value(projectId)" | xargs -P30 -I{} sh -c "gcloud storage buckets list --project={} --format=json | jq -s '[.[][]] | {project: \"{}\", storage_url: .[].storage_url, public_access_prevention: .[].public_access_prevention}'" | jq -c '.' | sort -u)
-      curr_private_buckets=$(echo "$current_all_buckets_json" | jq '. | select(.public_access_prevention == "enforced")' | jq -s '. | length')
+get_organizations() {
+    local org_id="$1"
 
-      curr_private_buckets_from_policy=$(echo "$current_all_buckets_json" | jq -r '. | select(.public_access_prevention != "enforced") | [.project,.storage_url] | join(" ")' | xargs -n2 -P30 sh -c 'if gcloud storage buckets get-iam-policy "$1" --project "$0" | grep -qE "\"allUsers\"|\"allAuthenticatedUsers\""; then
-    echo "0"
-else
-    [ $? -ne 0 ] && echo "Permission error to command \"gcloud storage buckets get-iam-policy\" for $1 in project $0, see $LOG_FILE logs for more details" >&2
-    echo "1"
-fi' | awk '{sum+=$1} END {print sum}')
-      total_private_buckets=$((total_private_buckets + curr_private_buckets + curr_private_buckets_from_policy))
-  fi
+    if [[ -z "$org_id" ]]; then
+        local orgs
+        orgs=$(gcloud organizations list --format="value(name)")
 
+        echo "$orgs"
+    else
+        echo "$org_id"
+    fi
+}
 
-  echo "$org_id Virtual Machines Count: $curr_vms"
-  echo "$org_id Serverless Functions Count: $curr_functions"
-  echo "$org_id Serverless Containers Count: $curr_cloud_run"
-  echo "$org_id Container Images Count: $curr_container_images"
-  echo "$org_id VM Images Count: $curr_vm_images"
-  echo "$org_id Container Hosts Count: $curr_gke_nodes"
-  echo "$org_id Private Buckets: $((curr_private_buckets + curr_private_buckets_from_policy))"
-  echo "$org_id Public Buckets: $((curr_buckets - curr_private_buckets))"
-  echo "$org_id Buckets Count: $curr_buckets"
-  echo "--------------------------------------"
-  echo "--------------------------------------"
-  counter=$((counter+1))
-  if [ -n "$ORGS_LEN" ]; then
-      echo -n "Progress: $counter/$ORGS_LEN organizations"
-  fi
+initialize_counters() {
+    for var in "${!RESOURCE_TYPES[@]}"; do
+        declare "${RESOURCE_TYPES[$var]}=0"
+    done
 
-  # Add a line break
-  echo -e "\n"
-done
+    total_private_buckets=0
+    total_managed_db_count=0
+}
 
-# Workloads calculation
-vm_workloads=$(( ( total_vms + WORKLOAD_VM_UNITS / 2 ) / WORKLOAD_VM_UNITS ))
-if [[ $vm_workloads -eq 0 && $total_vms -gt 0 ]]; then
-    vm_workloads=1
-fi
-function_workloads=$(( ( total_functions + WORKLOAD_FUNCTION_UNITS / 2 ) / WORKLOAD_FUNCTION_UNITS ))
-if [[ $function_workloads -eq 0 && $total_functions -gt 0 ]]; then
-    function_workloads=1
-fi
-container_workloads=$(( ( total_cloud_run + WORKLOAD_SERVERLESS_CONTAINER_UNITS / 2 ) / WORKLOAD_SERVERLESS_CONTAINER_UNITS ))
-if [[ $container_workloads -eq 0 && $total_cloud_run -gt 0 ]]; then
-    container_workloads=1
-fi
-container_image_workloads=$(( ( total_container_images + WORKLOAD_CONTAINER_IMAGE_UNITS / 2 ) / WORKLOAD_CONTAINER_IMAGE_UNITS ))
-if [[ $container_image_workloads -eq 0 && $total_container_images -gt 0 ]]; then
-    container_image_workloads=1
-fi
-vm_image_workloads=$(( ( total_vm_images + WORKLOAD_VM_IMAGE_UNITS / 2 ) / WORKLOAD_VM_IMAGE_UNITS ))
-if [[ $vm_image_workloads -eq 0 && $total_vm_images -gt 0 ]]; then
-    vm_image_workloads=1
-fi
-container_host_workloads=$(( ( total_gke_nodes + WORKLOAD_CONTAINER_HOST_UNITS / 2 ) / WORKLOAD_CONTAINER_HOST_UNITS ))
-if [[ $container_host_workloads -eq 0 && $total_gke_nodes -gt 0 ]]; then
-    container_host_workloads=1
-fi
-total_workloads=$(( vm_workloads + function_workloads + container_workloads + container_image_workloads + vm_image_workloads + container_host_workloads ))
+prepare_asset_types() {
+    local IFS=,
+    echo "${!RESOURCE_TYPES[*]}"
+}
 
-echo "=============="
-echo "Total results:"
-echo "=============="
-echo "Virtual Machines Count: $total_vms (Workload Units: ${vm_workloads})"
-echo "Serverless Functions Count: $total_functions (Workload Units: ${function_workloads})"
-echo "Serverless Containers Count: $total_cloud_run (Workload Units: ${container_workloads})"
-echo "Container Images Count: $total_container_images (Workload Units: ${container_image_workloads})"
-echo "VM Images Count: $total_vm_images (Workload Units: ${vm_image_workloads})"
-echo "Container Hosts Count: $total_gke_nodes (Workload Units: ${container_host_workloads})"
-echo "Private Buckets: $total_private_buckets"
-echo "Public Buckets: $((total_buckets - total_private_buckets))"
-echo "Buckets Count: $total_buckets"
-echo "--------------------------------------"
-echo "TOTAL Estimated Workload Units: ${total_workloads}"
-echo
-echo "Please verify if errors were encountered during the resource enumeration in the log file: ${LOG_FILE}"
+fetch_resources() {
+    local asset_types
+    local scope="$1"
+    asset_types=$(prepare_asset_types)
+
+    gcloud asset search-all-resources \
+        --scope="$scope" \
+        --format="json(displayName, assetType, parentFullResourceName)" \
+        --asset-types="$asset_types"
+}
+
+get_project_bucket_details() {
+    local project="$1"
+    gcloud storage buckets list --project="$project" --format=json |
+        jq -s "[.[][]] | {project: \"$project\", storage_url: .[].storage_url, public_access_prevention: .[].public_access_prevention}"
+}
+
+list_projects_by_type() {
+    local all_resources="$1"
+    local asset_type="$2"
+    jq -r --arg assetType "$asset_type" '.[] | select( .assetType == $assetType) | .parentFullResourceName | split("/")[4]' <<<"$all_resources"  | sort -u
+}
+
+count_private_buckets() {
+    local all_resources="$1"
+    local asset_type="storage.googleapis.com/Bucket"
+
+    local current_all_buckets_json
+    current_all_buckets_json=$(list_projects_by_type "$all_resources" "$asset_type" |
+        xargs -P30 -I{} bash -c "$(declare -f get_project_bucket_details); get_project_bucket_details '{}'" |
+        jq -c '.' | sort -u)
+
+    local curr_private_buckets
+    curr_private_buckets=$(echo "$current_all_buckets_json" |
+        jq '. | select(.public_access_prevention == "enforced")' |
+        jq -s '. | length')
+
+    local curr_private_buckets_from_policy
+    local all_non_enforced_buckets
+
+    all_non_enforced_buckets=$(echo "$current_all_buckets_json" | jq -r '. | select(.public_access_prevention != "enforced") | [.project,.storage_url] | join(" ")')
+
+    curr_private_buckets_from_policy=$(echo "$all_non_enforced_buckets" |
+        xargs -n2 -P30 bash -c "$(declare -f check_bucket_iam_policy); check_bucket_iam_policy \"\$0\" \"\$1\"" |
+        awk '{sum+=$1} END {print sum}')
+
+    echo $((curr_private_buckets + curr_private_buckets_from_policy))
+}
+
+count_managed_db_from_project() {
+    local project="$1"
+    gcloud -q sql instances list --project "${project}" --format json | jq -r --arg max_db_size_gb "$MAX_DB_SIZE_GB" '[.[] | select(.state == "RUNNABLE" and ((.settings.dataDiskSizeGb // "0") | tonumber) <= $max_db_size_gb)] | length'
+}
+
+count_all_managed_db() {
+    local all_resources="$1"
+    local current_managed_db_count=0
+    local asset_type="sqladmin.googleapis.com/Instance"
+
+    local projects
+    projects=$(list_projects_by_type "$all_resources" "$asset_type")
+
+    current_managed_db_count=$(echo "$projects" | xargs -n1 -P30 bash -c "$(declare -f count_managed_db_from_project); count_managed_db_from_project \"\$0\"" | awk '{sum+=$1} END {print sum}')
+    echo "$current_managed_db_count"
+}
+
+count_non_boot_disk_from_project() {
+    local project="$1"
+    gcloud compute instances list --project "${project}" --format json | jq -r '[.[] | select(.disks[].boot != true)] | length'
+}
+
+count_all_non_boot_disks() {
+    local all_resources="$1"
+    local asset_type="compute.googleapis.com/Instance"
+
+    local projects
+    projects=$(list_projects_by_type "$all_resources" "$asset_type")
+
+    local current_non_boot_disk_count=0
+    current_non_boot_disk_count=$(echo "$projects" | xargs -n1 -P30 bash -c "$(declare -f count_non_boot_disk_from_project); count_non_boot_disk_from_project \"\$0\"" | awk '{sum+=$1} END {print sum}')
+    echo "$current_non_boot_disk_count"
+}
+
+count_resources() {
+    local all_resources="$1"
+    local org_id="$2"
+
+    for asset_type in "${!RESOURCE_TYPES[@]}"; do
+        local var_name="${RESOURCE_TYPES[$asset_type]}"
+        local count
+
+        count=$(echo "$all_resources" | jq -r --arg type "$asset_type" \
+            '[.[] | select(.assetType == $type)] | length')
+
+        eval "$var_name=$((${!var_name} + count))"
+    done
+
+    local_private_buckets=$(count_private_buckets "$all_resources")
+    total_private_buckets=$((total_private_buckets + local_private_buckets))
+
+    local_managed_db_count=$(count_all_managed_db "$all_resources")
+    total_managed_db_count=$((total_managed_db_count + local_managed_db_count))
+}
+
+check_bucket_iam_policy() {
+    local project="$1"
+    local bucket="$2"
+
+    if gcloud storage buckets get-iam-policy "$bucket" --project "$project" |
+        grep -qE "\"allUsers\"|\"allAuthenticatedUsers\""; then
+        echo "0"
+    else
+        [ $? -ne 0 ] && echo "Permission error checking IAM policy for $bucket in project $project" >&2
+        echo "1"
+    fi
+}
+
+calculate_workloads() {
+    local total_count=$1
+    local workload_unit=$2
+
+    total_workload_unit_count=$(((total_count + workload_unit / 2) / workload_unit))
+    if [[ total_workload_unit_count -eq 0 && total_count -gt 0 ]]; then
+        total_workload_unit_count=1
+    fi
+    echo $total_workload_unit_count
+}
+
+main()  {
+    local org_id
+    org_id=$(parse_arguments "$@")
+    local orgs
+    orgs=$(get_organizations "$org_id")
+
+    initialize_counters
+
+    for org_id in $orgs; do
+        echo "Processing organization: $org_id"
+        local all_resources
+        all_resources=$(fetch_resources "organizations/$org_id")
+        count_resources "$all_resources" "$org_id"
+    done
+
+    calculate_resource_workloads
+
+    display_summary
+}
+
+main "$@"
